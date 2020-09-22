1fe00f4d783ecf0388e172781049ebe54e401572
unknown <developer.amel@gmail.com>
2020-09-22 16:50:42 +0200

    Added Context to project

--- App.js
+++ App.js
@@ -1,21 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import IndexScreen from "./src/screen/IndexScreen";
+import { BlogProvider } from "./src/context/BlogContext";
 
-export default function App() {
+const Stack = createStackNavigator();
+
+const App = () => (
+  <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Blogs"
+        component={IndexScreen}
+        options={{ title: "Blogs" }}
+      />
+    </Stack.Navigator>
+  </NavigationContainer>
+);
+
+export default () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <BlogProvider>
+      <App />
+    </BlogProvider>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+};
